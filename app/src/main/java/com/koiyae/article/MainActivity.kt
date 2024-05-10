05e5828c75267656cad0fd528935b198b46e41d7
koiyae <bruno.ryan.bs@gmail.com>
2024-05-10 15:43:24 -0300

    article

--- app/src/main/java/com/koiyae/article/MainActivity.kt
+++ app/src/main/java/com/koiyae/article/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,14 +52,17 @@ fun ArticlePage(title: String, text1: String, text2: String, modifier: Modifier 
         Text(
             text = title,
             fontSize = 24.sp,
+            textAlign = TextAlign.Justify,
             modifier = Modifier.padding(9.dp)
         )
         Text(
             text = text1,
+            textAlign = TextAlign.Justify,
             modifier = Modifier.padding(9.dp)
         )
         Text(
             text = text2,
+            textAlign = TextAlign.Justify,
             modifier = Modifier.padding(9.dp)
         )
     }
