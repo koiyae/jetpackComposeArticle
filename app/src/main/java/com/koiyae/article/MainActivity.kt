48a13b64fca7e08e60115d305d391d91885dbb9c
koiyae <bruno.ryan.bs@gmail.com>
2024-05-10 15:38:48 -0300

    article

---
+++ app/src/main/java/com/koiyae/article/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.koiyae.article
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.koiyae.article.ui.theme.ArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArticleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ArticlePage(
+                        title = stringResource(id = R.string.title),
+                        text1 = stringResource(id = R.string.text1),
+                        text2 = stringResource(id = R.string.text2),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArticlePage(title: String, text1: String, text2: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier){
+        val image = painterResource(id = R.drawable.bg_compose_background)
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(9.dp)
+        )
+        Text(
+            text = text1,
+            modifier = Modifier.padding(9.dp)
+        )
+        Text(
+            text = text2,
+            modifier = Modifier.padding(9.dp)
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ArticleTheme {
+//        Greeting("Android")
+//    }
+//}
